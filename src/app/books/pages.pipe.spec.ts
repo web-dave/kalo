36fc3981e752264486996170653cd4fc40e90ece
web-dave <dave@webdave.de>
2018-10-15 14:26:08 +0200

    pipe testing

--- src/app/books/pages.pipe.spec.ts
+++ src/app/books/pages.pipe.spec.ts
@@ -1,8 +1,12 @@
-import { PagesPipe } from './pages.pipe';
+import { PagesPipe } from "./pages.pipe";
 
-describe('PagesPipe', () => {
-  it('create an instance', () => {
+describe("PagesPipe", () => {
+  it("create an instance", () => {
     const pipe = new PagesPipe();
     expect(pipe).toBeTruthy();
+  });
+  it("should return Seitenzahl: 1000", () => {
+    const pipe = new PagesPipe();
+    expect(pipe.transform(1000, "Seitenzahl")).toBe("Seitenzahl: 1000");
   });
 });
