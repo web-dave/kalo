305dffc13c68791a8b84e48850f76025a483885b
web-dave <dave@webdave.de>
2018-10-15 14:11:42 +0200

    directive testing

--- src/app/books/order-btn.directive.spec.ts
+++ src/app/books/order-btn.directive.spec.ts
@@ -1,4 +1,24 @@
-import { OrderBtnDirective } from './order-btn.directive';
+import { OrderBtnDirective } from "./order-btn.directive";
+
+@Component({
+  selector: "",
+  template: "<div [orderBtn]='book'></div>"
+})
+class Foo {
+  book = {
+    title: "Design Patterns",
+    subtitle: "Elements of Reusable Object-Oriented Software",
+    isbn: "978-0-20163-361-0",
+    abstract:
+      "Capturing a wealth of experience about the design of object-oriented software, four top-notch designers present a catalog of simple and succinct solutions to commonly occurring design problems. Previously undocumented, these 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves.",
+    numPages: 395,
+    author: "Erich Gamma / Richard Helm / Ralph E. Johnson / John Vlissides",
+    publisher: {
+      name: "Addison-Wesley",
+      url: "http://www.addison-wesley.de/"
+    }
+  };
+}
 
 // describe('OrderBtnDirective', () => {
 //   it('should create an instance', () => {
