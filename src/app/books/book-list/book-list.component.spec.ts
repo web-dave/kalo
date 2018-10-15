823bda196f6f6d6ceaa16bcb313f7d9cba5550da
web-dave <dave@webdave.de>
2018-10-15 11:49:43 +0200

    Booklist test

--- src/app/books/book-list/book-list.component.spec.ts
+++ src/app/books/book-list/book-list.component.spec.ts
@@ -3,18 +3,57 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { BookListComponent } from "./book-list.component";
 import { BookService } from "../../shared/book.service";
 import { BookDetailsComponent } from "../book-details/book-details.component";
+import {
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+  Directive
+} from "@angular/core";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { of } from "rxjs";
+import { BookServiceMock } from "src/app/shared/serv.mock";
 
-describe("BookListComponent", () => {
+@Component({
+  selector: "app-book-preview",
+  template: ""
+})
+class Foo {
+  @Input()
+  book;
+  @Input()
+  change: string;
+  @Output()
+  bookselected = new EventEmitter();
+}
+
+@Directive({
+  selector: "[orderBtn]"
+})
+class Bar {
+  @Input()
+  orderBtn;
+}
+
+fdescribe("BookListComponent", () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BookListComponent, BookDetailsComponent],
-      providers: [BookService]
+      declarations: [BookListComponent, BookDetailsComponent, Foo, Bar],
+      // providers: [BookService],
+      providers: [
+        {
+          provide: BookService,
+          useClass: BookServiceMock
+        }
+      ],
+      imports: [RouterTestingModule]
     }).compileComponents();
-    let serv = TestBed.get(BookService);
-    spyOn(serv, "getBooks").and.returnValue([]);
+    // let serv = TestBed.get(BookService);
+    // spyOn(serv, "getBooks").and.returnValue(of([]));
   }));
 
   beforeEach(() => {
